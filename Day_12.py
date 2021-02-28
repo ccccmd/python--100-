73bda3bae0c15e17b5ed88c65122ec238fa441f6
ccccmd <chenmengda2009@163.com>
2021-02-28 23:13:26 +0800

    Day_12

--- Day_12.py
+++ Day_12.py
@@ -1,1 +1,16 @@
 # -*- coding:utf-8 -*-
+# 判断101-200之间有多少个素数，并输出所有素数
+import math
+def prim(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return 1
+    return 0
+sum = 0
+for i in range(101, 200):
+    if prim(i) == 0:
+        sum = sum + 1
+        print(i, end=" ")
+
+print()
+print("共有%d个素数" %sum)
