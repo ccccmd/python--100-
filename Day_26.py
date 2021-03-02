976292780aa6beba2bcfc240dae9fdb2844e2de0
ccccmd <chenmengda2009@163.com>
2021-03-03 00:28:21 +0800

    Day_26

---
+++ Day_26.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# 求1!+2!+...20!的和
+
+def jiecheng(n):
+    if n != 1:
+        return n * jiecheng(n - 1)
+    else:
+        return 1
+sum = 0
+for i in range(1, 21):
+    sum += jiecheng(i)
+print(sum)
