a73660d94a67198b751d3709603d764eb62add6b
ccccmd <chenmengda2009@163.com>
2021-03-03 09:18:52 +0800

    Day_27

--- Day_27.py
+++ Day_27.py
@@ -1,2 +1,8 @@
 # -*- coding:utf-8 -*-
-# 利用递归函数调用方式，jiangjiang
+# 利用递归函数调用方式，将所输入的5个字符，以相反顺序打印出来
+str = input("输入字符")
+def f(x):
+    if x == -1:
+        return ''
+    return str[x] + f(x - 1)
+print(f(len(str) - 1))
